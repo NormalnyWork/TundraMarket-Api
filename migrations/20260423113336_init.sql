32f0f8ac87a74ce231f23e31ac356d06e54b9845
sightless <Twinkg06@mail.ru>
2026-04-23 23:54:35 +0500

    style: indentation

--- migrations/20260423113336_init.sql
+++ migrations/20260423113336_init.sql
@@ -1,21 +1,21 @@
 -- +goose Up
 CREATE TABLE nomad (
-    id SERIAL PRIMARY KEY,
-    phone VARCHAR(25) NOT NULL UNIQUE,
-    longitude DECIMAL(9, 6),
-    latitude DECIMAL(9, 6),
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
+    id          SERIAL      PRIMARY KEY,
+    phone       VARCHAR(25) NOT NULL UNIQUE,
+    longitude   DECIMAL(9, 6),
+    latitude    DECIMAL(9, 6),
+    created_at  TIMESTAMP   NOT NULL DEFAULT NOW(),
+    updated_at  TIMESTAMP   DEFAULT NOW()
 );
 
 CREATE TABLE trading_station (
-    id SERIAL PRIMARY KEY,
-    phone VARCHAR(25) UNIQUE,
-    name VARCHAR(255),
-    longitude DECIMAL(9, 6),
-    latitude DECIMAL(9, 6),
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
+    id          SERIAL      PRIMARY KEY,
+    phone       VARCHAR(25) UNIQUE,
+    name        VARCHAR(255),
+    longitude   DECIMAL(9, 6),
+    latitude    DECIMAL(9, 6),
+    created_at  TIMESTAMP   NOT NULL DEFAULT NOW(),
+    updated_at  TIMESTAMP   DEFAULT NOW()
 );
 
 CREATE TYPE status AS ENUM(
@@ -28,14 +28,14 @@ CREATE TYPE status AS ENUM(
 );
 
 CREATE TABLE orders (
-    id SERIAL PRIMARY KEY,
-    nomad_id INT REFERENCES nomad(id) ON DELETE CASCADE,
-    trading_station_id INT REFERENCES trading_station(id) ON DELETE CASCADE,
-    status status NOT NULL DEFAULT 'CREATED',
-    longitude DECIMAL(9, 6),
-    latitude DECIMAL(9, 6),
-    comment VARCHAR(255),
-    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+    id                  SERIAL      PRIMARY KEY,
+    nomad_id            INT         REFERENCES nomad(id) ON DELETE CASCADE,
+    trading_station_id  INT         REFERENCES trading_station(id) ON DELETE CASCADE,
+    status              status      NOT NULL DEFAULT 'CREATED',
+    longitude           DECIMAL(9, 6),
+    latitude            DECIMAL(9, 6),
+    comment             VARCHAR(255),
+    created_at          TIMESTAMP   NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX idx_orders_nomad_id ON orders(nomad_id);
@@ -43,31 +43,31 @@ CREATE INDEX idx_orders_station_id ON orders(trading_station_id);
 CREATE INDEX idx_orders_status ON orders(status);
 
 CREATE TABLE product (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR,
-    price INT,
-    weight FLOAT,
-    volume FLOAT,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+    id          SERIAL      PRIMARY KEY,
+    name        VARCHAR,
+    price       INT,
+    weight      FLOAT,
+    volume      FLOAT,
+    created_at  TIMESTAMP   NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE orders_to_product (
-    id SERIAL PRIMARY KEY,
-    orders_id INT REFERENCES orders(id) ON DELETE CASCADE,
-    product_id INT REFERENCES product(id) ON DELETE CASCADE,
+    id          SERIAL      PRIMARY KEY,
+    orders_id   INT         REFERENCES orders(id) ON DELETE CASCADE,
+    product_id  INT         REFERENCES product(id) ON DELETE CASCADE,
     UNIQUE (orders_id, product_id),
-    quantity INT NOT NULL DEFAULT 1,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+    quantity    INT         NOT NULL DEFAULT 1,
+    created_at  TIMESTAMP   NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX idx_otp_orders_id ON orders_to_product(orders_id);
 CREATE INDEX idx_otp_product_id ON orders_to_product(product_id);
 
 CREATE TABLE status_history (
-    id SERIAL PRIMARY KEY,
-    orders_id INT REFERENCES orders(id) ON DELETE CASCADE,
-    status status,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+    id          SERIAL      PRIMARY KEY,
+    orders_id   INT         REFERENCES orders(id) ON DELETE CASCADE,
+    status      status,
+    created_at  TIMESTAMP   NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX idx_status_history_order_id ON status_history(orders_id);
